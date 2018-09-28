ca0c65a336ec7125de96f429ecb030e3305a8525
José Carlos Muñoz Méndez <jmunozm@alsea.com.mx>
2018-09-27 20:17:22 -0500

    Passed parameters

--- main.go
+++ main.go
@@ -1,65 +1,72 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
 	"image"
-	"image/color"
 	"image/draw"
-	"image/png"
+	"image/jpeg"
 	"io/ioutil"
 	"log"
 	"os"
 
-	"golang.org/x/image/font"
-
 	"github.com/golang/freetype"
+	"golang.org/x/image/font"
 )
+
+func init() {
+	image.RegisterFormat("jpeg", "jpeg", jpeg.Decode, jpeg.DecodeConfig)
+}
 
 var (
 	wonb     = flag.Bool("whiteonblack", false, "white text on black background")
 	fontfile = flag.String("fontfile", "./resources/font/AvenirLTStd-Heavy.ttf", "filename of the ttf font")
 	dpi      = flag.Float64("dpi", 72, "screen resolution")
-	hinting  = flag.String("hinting", "none", "none|full")
-	size     = flag.Float64("size", 12, "font size in points")
+	hinting  = flag.String("hinting", "full", "none|full")
+	size     = flag.Float64("size", 86, "font size in points")
 	spacing  = flag.Float64("spacing", 1.5, "line spacing")
+	name     = flag.String("name", "Carlos Muñoz", "Nombre del Usuario")
 )
 
-var name = "Carlos Muñoz"
+//var name = "Carlos Muñoz"
 
 func main() {
+	goldCard, err := os.Open("./resources/goldcard.jpg")
 	flag.Parse()
-	fontBytes, err := ioutil.ReadFile(*fontfile)
+	log.Print(*name)
 
 	if err != nil {
-		log.Println(err)
-		return
+		fmt.Println("la imagen no fue encontrada")
+		os.Exit(1)
+	}
+
+	fontBytes, err := ioutil.ReadFile(*fontfile)
+	if err != nil {
+		fmt.Println("la fuente no fue encontrada")
+		os.Exit(1)
 	}
 
 	f, err := freetype.ParseFont(fontBytes)
 	if err != nil {
-		log.Println(err)
+		fmt.Println(err)
 		return
 	}
 
-	//initialize context
-	fg, bg := image.Black, image.White
-	ruler := color.RGBA{0xdd, 0xdd, 0xdd, 0xff}
+	defer goldCard.Close()
 
-	if *wonb {
-		fg, bg = image.White, image.Black
-		ruler = color.RGBA{0x22, 0x22, 0x22, 0xff}
-	}
+	//bg := image.Black
 
-	rgba := image.NewRGBA(image.Rect(0, 0, 640, 480)) // generates a new canvas i guess
-	draw.Draw(rgba, rgba.Bounds(), bg, image.ZP, draw.Src)
+	img, _, err := image.Decode(goldCard) // abre la imagen
+	rgba := image.NewRGBA(image.Rect(0, 0, 948, 597))
+	//draw.Draw(rgba, rgba.Bounds(), bg, image.ZP, draw.Src)
+	draw.Draw(rgba, rgba.Bounds(), img, image.ZP, draw.Src)
 	c := freetype.NewContext()
 	c.SetDPI(*dpi)
 	c.SetFont(f)
 	c.SetFontSize(*size)
 	c.SetClip(rgba.Bounds())
 	c.SetDst(rgba)
+	fg := image.White
 	c.SetSrc(fg)
 
 	switch *hinting {
@@ -69,41 +76,57 @@ func main() {
 		c.SetHinting(font.HintingFull)
 	}
 
-	// dibuja las líneas guía
-	for i := 0; i > 200; i++ {
-		rgba.Set(10, 10+i, ruler)
-		rgba.Set(10+i, 10, ruler)
-	}
+	//****** dibuja el texto *****//
 
-	//dibuja el texto
-
-	pt := freetype.Pt(10, 10+int(c.PointToFixed(*size)>>6))
-	_, err = c.DrawString(name, pt)
+	pt := freetype.Pt(42, 128+int(c.PointToFixed(*size)>>6))
+	_, err = c.DrawString(*name, pt)
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	//SAVE FILE TO DISK
-	outFile, err := os.Create("out.png")
+	/* ** guarda la imagen a disco ** */
+	outFile, err := os.Create("out.jpg")
 	if err != nil {
 		log.Println(err)
 		os.Exit(1)
 	}
 
 	defer outFile.Close()
-	b := bufio.NewWriter(outFile)
-	err = png.Encode(b, rgba)
+	//b := bufio.NewWriter(outFile)
+
+	/* ENCODING IN PNG ************************
+	var Enc png.Encoder
+	//set the best compression
+	Enc.CompressionLevel = png.BestCompression
+
+	err = Enc.Encode(b, rgba)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	******************************************/
+
+	/**********************************
+	**** Encoding in JPG
+	**********************************/
+	//var Quality = 40
+	//var b bytes.Buffer
+	var opt jpeg.Options
+	opt.Quality = 40
+
+	err = jpeg.Encode(outFile, rgba, &opt)
 	if err != nil {
 		log.Println(err)
 		os.Exit(1)
 	}
 
-	err = b.Flush()
+	/*err = b.Flush()
 	if err != nil {
 		log.Println(err)
 		os.Exit(1)
-	}
+	}*/
 
-	fmt.Println("ya tienes tu archivo")
+	fmt.Println("archivo escrito")
+
 }
